f4e1d1ffaf013109b5c482210287291d517236be
Kristan Ruíz <itan_07@hotmail.com>
2023-09-01 16:25:30 -0800

    Simplificando el menu, menos opciones

--- forms/menu.Designer.cs
+++ forms/menu.Designer.cs
@@ -30,18 +30,16 @@
         {
             label1 = new Label();
             buttonJugar = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
             buttonSalir = new Button();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(109, 34);
+            label1.Location = new Point(113, 20);
             label1.Name = "label1";
             label1.Size = new Size(136, 20);
             label1.TabIndex = 0;
@@ -49,7 +47,7 @@
             // 
             // buttonJugar
             // 
-            buttonJugar.Location = new Point(71, 207);
+            buttonJugar.Location = new Point(137, 206);
             buttonJugar.Name = "buttonJugar";
             buttonJugar.Size = new Size(94, 29);
             buttonJugar.TabIndex = 1;
@@ -57,54 +55,9 @@
             buttonJugar.UseVisualStyleBackColor = true;
             buttonJugar.Click += buttonJugar_Click;
             // 
-            // button2
-            // 
-            button2.Location = new Point(200, 180);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 2;
-            button2.Text = "Continuar";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(200, 235);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 29);
-            button3.TabIndex = 3;
-            button3.Text = "Nueva Partida";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(71, 517);
-            button4.Name = "button4";
-            button4.Size = new Size(94, 29);
-            button4.TabIndex = 4;
-            button4.Text = "Importar";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(209, 517);
-            button5.Name = "button5";
-            button5.Size = new Size(94, 29);
-            button5.TabIndex = 5;
-            button5.Text = "Exportar";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(151, 301);
-            button6.Name = "button6";
-            button6.Size = new Size(94, 29);
-            button6.TabIndex = 6;
-            button6.Text = "Ajustes";
-            button6.UseVisualStyleBackColor = true;
-            // 
             // buttonSalir
             // 
-            buttonSalir.Location = new Point(269, 596);
+            buttonSalir.Location = new Point(137, 254);
             buttonSalir.Name = "buttonSalir";
             buttonSalir.Size = new Size(94, 29);
             buttonSalir.TabIndex = 7;
@@ -112,21 +65,36 @@
             buttonSalir.UseVisualStyleBackColor = true;
             buttonSalir.Click += buttonSalir_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(113, 60);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(138, 124);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(7, 348);
+            label2.Name = "label2";
+            label2.Size = new Size(205, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Hecho por: Kristan Ruíz limón";
+            // 
             // menu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(390, 646);
+            ClientSize = new Size(377, 378);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(buttonSalir);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
             Controls.Add(buttonJugar);
             Controls.Add(label1);
             Name = "menu";
             Text = "menu";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -135,11 +103,8 @@
 
         private Label label1;
         private Button buttonJugar;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
         private Button buttonSalir;
+        private PictureBox pictureBox1;
+        private Label label2;
     }
 }
